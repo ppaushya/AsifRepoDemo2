488fcfa92cf32b774ecdd3c52fdd4503a0ec0cca
muasif <muasif@DIN73003011.corp.capgemini.com>
2018-11-23 16:51:39 +0530

    a

--- Day1_Spring_Demo/src/main/java/org/cap/boot/TestClass.java
+++ Day1_Spring_Demo/src/main/java/org/cap/boot/TestClass.java
@@ -24,18 +24,7 @@ public class TestClass {
 		context.close();
 		//context.registerShutdownHook();
 		System.out.println("hello how are you!!");
-		System.out.println("hello how are you!!");
-		System.out.println("you are how hello!!");
 		
-		System.out.println("No but you are ...!");
-		System.out.println("No but you are ...!");
-		System.out.println("hello how are you!!");
-		System.out.println("Poonam");
-		System.out.println("Asif");
-		System.out.println("Asif");
-		
-		System.out.println("Poonam");
-		System.out.println("Poonam pagal hai");
 
 
 	}
