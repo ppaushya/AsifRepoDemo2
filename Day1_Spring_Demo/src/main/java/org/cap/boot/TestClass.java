e6a4f8966d492750ca55259119f203ad9ed264c8
muasif <muasif@DIN73003011.corp.capgemini.com>
2018-11-23 16:51:10 +0530

    as

--- Day1_Spring_Demo/src/main/java/org/cap/boot/TestClass.java
+++ Day1_Spring_Demo/src/main/java/org/cap/boot/TestClass.java
@@ -26,7 +26,7 @@ public class TestClass {
 		System.out.println("hello how are you!!");
 		System.out.println("hello how are you!!");
 		System.out.println("you are how hello!!");
-		System.out.println("Are you in love? Asif");
+		
 		System.out.println("No but you are ...!");
 		System.out.println("No but you are ...!");
 		System.out.println("hello how are you!!");
